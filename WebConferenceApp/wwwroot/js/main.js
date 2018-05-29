8094dc3927dcbd00a6addedb3bc92bdafc4d5b47
chii chobits <velikiinyashko@live.ru>
2018-05-29 10:00:09 +0700

    update 29.05.18

--- WebConferenceApp/wwwroot/js/main.js
+++ WebConferenceApp/wwwroot/js/main.js
@@ -1,51 +1,21 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
-
 'use strict';
 
-var errorElement = document.querySelector('#errorMsg');
-var video = document.querySelector('video');
+var startButton = document.getElementById('startButton');
+var callButton = document.getElementById('callButton');
 
-// Put variables in global scope to make them available to the browser console.
-var constraints = window.constraints = {
-  audio: false,
-  video: true
-};
+var videoLocal = document.querySelector('video#videoLocal');
+var videoRemove = document.querySelector('video#videoRemove');
 
-function handleSuccess(stream) {
-  var videoTracks = stream.getVideoTracks();
-  console.log('Got stream with constraints:', constraints);
-  console.log('Using video device: ' + videoTracks[0].label);
-  stream.oninactive = function() {
-    console.log('Stream inactive');
-  };
-  window.stream = stream; // make variable available to browser console
-  video.srcObject = stream;
+startButton.onclick = start;
+
+function start() {
+    console.log(' press start button');
+    startButton.disablet = true;
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    })
+        .catch(function (e) {
+            console.log('getUserMedia() error: ', e);
+        });
 }
-
-function handleError(error) {
-  if (error.name === 'ConstraintNotSatisfiedError') {
-    errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
-        constraints.video.width.exact + ' px is not supported by your device.');
-  } else if (error.name === 'PermissionDeniedError') {
-    errorMsg('Permissions have not been granted to use your camera and ' +
-      'microphone, you need to allow the page access to your devices in ' +
-      'order for the demo to work.');
-  }
-  errorMsg('getUserMedia error: ' + error.name, error);
-}
-
-function errorMsg(msg, error) {
-  errorElement.innerHTML += '<p>' + msg + '</p>';
-  if (typeof error !== 'undefined') {
-    console.error(error);
-  }
-}
-
-navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
